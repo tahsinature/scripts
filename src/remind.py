e8296441b8ddc481bbc29eeb6b71ffd5810c569d
Tahsin <work.tahsin@gmail.com>
2024-03-23 15:07:31 -0400

    update reminder

--- src/remind.py
+++ src/remind.py
@@ -1,59 +1,70 @@
-import time
 import json
 import click
 
-from utilities.datetime import convert_to_different_tz
+from utilities.datetime import calculate_time, convert_to_different_tz
 from utilities.dependency import check_dependencies
 from utilities.openai import ask_gpt_3_with_chat_completions
 from utilities.system import run_terminal_command
+from utilities.log import Log
 
 
 check_dependencies(["reminders"], silent=True)
-
-
-now = time.time()
 my_timezone = "America/New_York"
-expected_formats = "DD MMM YYYY HH:MM:SS AM/PM"  # 15 Sep 2022 03:45:20 PM
-current_formatted_time = time.strftime("%d %b %Y %I:%M:%S %p", time.localtime(now))
-
-system_prompt = f"""You're a helpful AI assistant that will take user input and try to extract a time and title from it. If there is no time, give me just the title.
-For the time, I want it in this formats: {expected_formats}. Whichever fits best for the given input.
-Now in my system time, it's {current_formatted_time}. My timezone is EST. I'm giving you current time so if user says "remind me in 5 minutes", you can calculate the time.
-User might ask you in different timezones, so you will try your best to get the timezon. ex: 'America/Los_Angeles'. If you're confused about the time today or tomorrow, see if the requested time location is in the future or past.
-Let's say My system time is 21 Mar 2024 03:14 pm. If user says "remind me to wake her up at 4 am indonesia time", you should understand in indonesia it's already 22 Mar 2024. So you should set the reminder for 22 Mar 2024 4 am.
-
-And for the title, try to format it nicely. If it's "remind me to go to the grocery store", just give me "Go to the grocery store".
+reminder_list = "Remind"
 
 
-Give me json with the title, time & timezone. If there is no time, just give me the title.
-Expected output-1: {{"title": "title", "time": "20 Sep 2023 10:00 pm", "timezone": "America/Los_Angeles"}}
-Expected output-2: {{"title": "title", "time": null, "timezone": null}}
-Expected output-3: {{"title": null, "time": null}}
+system_prompt = """You're a helpful AI assistant that will take user input and try to extract a title & time-data from it. If there is no time involved, give me just the title.
+Your json response format should be like this:
+    - {{"title":"title","when":{"time":"10:00 pm","timezone":"America/Los_Angeles","date":"20 Sep 2023","date_type":"absolute"}}}
+    - {{"title":"title","when":{"time":"10:00 pm","timezone":"America/Los_Angeles","date":1,"date_type":"relative"}}} // unit: days. 0 means no date / today. 1 means tomorrow. -1 means yesterday / 5 means 5 days from now / -5 means 5 days ago and so on.
+when {
+    time: "python %I:%M %p" # required # do your best guess and put in proper format. If no time, just put "00:00 am"
+    timezone: "America/Los_Angeles" # required # Default: America/New_York
+    date: "DD MMM YYYY" | int # required
+    date_type: "absolute" | "relative" # required
+}
+You will give me minified json response.
 """
+
+log = Log("remind")
 
 
 @click.command()
 @click.argument("input", type=str)
 def exec(input: str):
+    log.group(f"input: {input}")
     res = ask_gpt_3_with_chat_completions(system_prompt, input, "json")
+    print(res)
+    log.group(f"api_res: {res}")
 
     if res and type(res) == str:
         dict_res = json.loads(res)
         title = dict_res.get("title")
-        requested_tz = dict_res.get("timezone")
+        when = dict_res.get("when")
+
         if not title:
             print("Title is required")
             return
-        time = dict_res.get("time")
-        command = f"reminders add Remind {title}"
-        if time and requested_tz:
-            result = convert_to_different_tz(time, requested_tz, my_timezone)
+
+        command = f"reminders add {reminder_list} {title}"
+
+        if when:
+            requested_tz = when.get("timezone")
+            time = when.get("time")
+            date = when.get("date")
+            date_type = when.get("date_type")
+            actual_date = calculate_time(time, requested_tz, date, date_type)
+            result = convert_to_different_tz(actual_date, my_timezone)
             command += f""" --due-date "{result}" """
 
+        log.group(f"command: {command}")
         output = run_terminal_command(command, shell=True)
+        log.group(f"output: {output}")
         print(output)
     else:
         raise Exception("Invalid response from GPT-3")
+
+    log.end_group()
 
 
 if __name__ == "__main__":
