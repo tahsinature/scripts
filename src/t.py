f043b44e0f3c67d49918208b1cbfbe23e8f3bd76
Tahsin <work.tahsin@gmail.com>
2024-03-26 06:53:54 -0400

    test md renderer

--- src/t.py
+++ src/t.py
@@ -1,34 +1,39 @@
 #!/bin/env python3
 
-from utilities.log import Log
+from datetime import timedelta
+from utilities.datetime import try_to_parse_date_or_time
 import click
-import subprocess
 import json
-# import logging
 
-# logger1 = logging.getLogger("test")
-# logger1.setLevel(logging.DEBUG)
-# file_handler = logging.FileHandler("test.log")
-# file_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-# logger1.addHandler(file_handler)
-# logger1.debug("Starting the process.")
+from utilities.log import display_markdown
+from utilities.prompt import ask_confirmation
+from utilities.system import run_terminal_command
 
 
-log1 = Log("test.log")
-log2 = Log("test.log")
+text_data = """
+Python does not have a built-in switch case statement like some other programming languages. However, you can achieve similar functionality using a dictionary. Here's an example:
+
+```python
+def switch_case(argument):
+    switcher = {
+        1: "Case 1",
+        2: "Case 2",
+        3: "Case 3"
+    }
+
+    return switcher.get(argument, "Invalid case")
+
+# Test the function
+print(switch_case(1))  # Output: Case 1
+print(switch_case(4))  # Output: Invalid case
+```
+
+In this example, the `switch_case` function emulates a switch case statement by using a dictionary where keys represent cases and values represent corresponding actions. The `get` method is used to retrieve the value for a given key, falling back to a default value if the key is not found."""
 
 
 @click.command()
 def main():
-    log1.group("Starting the process.")
-    log1.group("Checking the dependencies.")
-    log1.group("Doing something.")
-    log1.end_group()
-
-    log2.group("Starting the process 2.")
-    log2.group("Checking the dependencies 2.")
-    log2.group("Doing something 2.")
-    log2.end_group()
+    display_markdown(text_data)
 
 
 if __name__ == "__main__":
