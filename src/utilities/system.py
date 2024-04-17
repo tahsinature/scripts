57e78344421eee877c59f22c7c4b201be1dd6958
Tahsin <work.tahsin@gmail.com>
2024-04-17 09:54:45 -0400

    add live command utility

--- src/utilities/system.py
+++ src/utilities/system.py
@@ -9,3 +9,17 @@ def run_terminal_command(command: str, shell: bool = False) -> str:
         raise Exception(stderr.decode("utf-8").strip())
 
     return stdout.decode("utf-8").strip()
+
+
+def run_terminal_command_live(command: str, shell: bool = False):
+    process = subprocess.Popen(command, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    while True and process.stdout:
+        output = process.stdout.readline().decode("utf-8").strip()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output)
+
+    rc = process.poll()
+    return rc
